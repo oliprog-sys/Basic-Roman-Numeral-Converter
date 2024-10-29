a71a45663305d284d89d4460d7bfd07ec39c1d7a
oliprog-sys <oliyaddiriba75@gmail.com>
2024-10-29 09:28:33 +0300

    added function to convert the numeric number to roman number

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
 const numberInput = document.getElementById("number");
 const convertBtn = document.getElementById("convert-btn");
-const resultOuput = document.getElementById("output");
+const resultOutput = document.getElementById("output");
 
 const checkUserInput = () => {
     const inputNumber = parseInt(numberInput.value);
 
-    if(!numberInput.value){
-        alert("Please enter a valid number");
-    } else if(inputNumber < 0){
-        alert("Please enter a number greater than or equal to 1");        
-    } else if(inputNumber >= 4000){
-        alert("Please enter a number less than or equal to 3999");
+    if(!numberInput.value){        
+        resultOutput.textContent = "Please enter a valid number";
+        return;
+    } else if(inputNumber < 0){       
+        resultOutput.textContent = "Please enter a number greater than or equal to 1";
+        return;
+    } else if(inputNumber >= 4000){    
+        resultOutput.textContent = "Please enter a number less than or equal to 3999";
+        return;
     }
 
+    resultOutput.textContent = numeralToRoman(inputNumber);    
     numberInput.value="";
 }
 
 const numeralToRoman = (input) => {
+    let romanNum='';
+    const values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    const symbols = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
 
+    for(let i = 0; i < values.length; i++){
+        while(input >= values[i]){
+            romanNum += symbols[i];
+            input -= values[i];
+        }
+    }
+
+    return romanNum;
 }
 
 convertBtn.addEventListener("click", checkUserInput);
